f0c2976583b3d22be84548901bdfc09f018de2f1
Jim Schneidereit <jimschneidereit@gmail.com>
2016-02-14 18:08:39 -0800

    making reduction method executable from tstl

--- projects/schnejam/reductions.py
+++ projects/schnejam/reductions.py
@@ -2,45 +2,51 @@ from __future__ import print_function
 import numpy as np
 import pyopencl as cl
 import pyopencl.array
-from pyopencl.reduction import ReductionKernel
-from numpy.random import RandomState
+import pyopencl.reduction
 
-expr = {"+" : lambda x, y: x + y,
-        "-" : lambda x, y: x - y,
-        "*" : lambda x, y: x * y,
-        "/" : lambda x, y: x / y}
+reduction_actions = {"+": lambda x, y: x + y,
+                     "-": lambda x, y: x - y,
+                     "*": lambda x, y: x * y,
+                     "/": lambda x, y: x / y}
 
-def PyOpenCLSumReduce():
-    '''
-    Prooof of concept for PyOpenCL to guide testing
-    '''
-    rand = RandomState(seed=42)
-    vals = rand.randint(-10, 10, 1024)
+
+def pyopencl_reduction(seed=42, length=3, reduction="+", min_endpoint=0, max_endpoint=5):
+    """
+    Proof of concept for PyOpenCL to guide testing
+    :param seed: Seed for repeatability
+    :param length: Length of test array
+    :param reduction: Type of reduction to be performed, see expr
+    :param min_endpoint: Lower bound of random values
+    :param max_endpoint: Upper bound of random values
+    :return: Nothing
+    """
+    assert isinstance(seed, int) and isinstance(length, int)
+    assert isinstance(min_endpoint, int) and isinstance(max_endpoint, int)
+    assert reduction in reduction_actions.keys()
+
+    rand = np.random.RandomState(seed=seed)
+    cpu_data = rand.randint(min_endpoint, max_endpoint, length).astype(np.int32)
 
     ctx = cl.create_some_context()
     queue = cl.CommandQueue(ctx)
+    gpu_data = pyopencl.array.to_device(queue, cpu_data)
 
-    a = pyopencl.array.Array(cqa=queue, shape=vals.shape, dtype=vals.dtype)
-    a.set(ary=vals)
+    kernel = pyopencl.reduction.ReductionKernel(ctx=ctx,
+                                                dtype_out=np.int32,
+                                                neutral="0",
+                                                reduce_expr="a+b".format(reduction),
+                                                map_expr="x[i]",
+                                                arguments="__global int *x")
+    clresult = kernel(gpu_data).get()
     queue.finish()
 
-    kernel = ReductionKernel(ctx=ctx,
-                             dtype_out=np.int32,
-                             neutral="0",
-                             reduce_expr="a+b",
-                             map_expr="x[i]",
-                             arguments="__global int *x")
-
-    clresult = kernel(a).get()
-    queue.finish()
-
-    pyresult = reduce(lambda x, y: x + y, vals)  # could also use np.sum(vals) for this case
+    pyresult = reduce(reduction_actions[reduction], cpu_data)
     print("clresult: {0}, pyresult: {1}".format(clresult, pyresult))
-    assert clresult == pyresult
+    return clresult == pyresult
 
 
 def main():
-    PyOpenCLSumReduce()
+    print(pyopencl_reduction(seed=42))
 
 
 if __name__ == '__main__':
