ec74a6c07720ad4d88ddca820394930b1bd17e35
Spencer Hubbard <hubbardspencerm@gmail.com>
2016-02-18 23:40:07 -0800

    Finished basic test harness and added to documentation

--- projects/hubbarsp/mysqlparse.py
+++ projects/hubbarsp/mysqlparse.py
@@ -1,8 +1,9 @@
-# this is a temp file for learning sqlparse
+# This module contains helper functions used by testing harness for sqlparse.
 import sqlparse
 from sqlparse import tokens
 from sqlparse import compat
 
+# Builds a pretty print string for the given parse tree.
 def pretty(parsed):
   def helper(parsed, i = 0):
     result = '%s%s \'%s\'\n' % (' ' * (2 * i), parsed._get_repr_name(), parsed._get_repr_value())
@@ -12,20 +13,15 @@ def pretty(parsed):
     return result
   return '\n' + helper(parsed)
 
-def walk(parsed):
-  return parsed
-
-def forwards(stmt, i = 0):
-  return sqlparse.parse(stmt)[i]
-
-def backwards(parsed):
-  return compat.u(parsed)
-
 if __name__ == '__main__':
-  parsed1 = forwards('select * from foo where bar = "baz";')
-  parsed2 = forwards('select A.a, A.b from A;')
-  parsed3 = forwards('select a, b from (select * from A where c < 1);')
+  stmt1 = 'select * from foo where bar = "baz";'
+  stmt2 = 'select A.a, A.b from A;'
+  stmt3 = 'select a, b from (select * from A where c < 1);'
+  parsed1 = sqlparse.parse(stmt1)[0]
+  parsed2 = sqlparse.parse(stmt2)[0]
+  parsed3 = sqlparse.parse(stmt3)[0]
   print pretty(parsed1)
   print pretty(parsed2)
   print pretty(parsed3)
-  print backwards(parsed1)
+  print compat.u(parsed1) == stmt1
+  print parsed1.get_type() == 'SELECT'
