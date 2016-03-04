b7bebb07bc6bb6cf29c366555d066a5dd2af1e03
Sean Penney <seanjpenney@gmail.com>
2016-03-03 19:59:01 -0800

    part 3

--- projects/penneys/myDict.py
+++ projects/penneys/myDict.py
@@ -8,16 +8,36 @@ class Dict(dict):
 		self._dict[id] = val
 		
 	def get(self, id):
-		return self._dict[id]
+		if id in self._dict:
+			return self._dict[id]
+		else:
+			return None
 		
 	def getSize(self):
 		return len(self._dict)
 
 	def getMax(self):
-		return max(self._dict.iteritems(), key=operator.itemgetter(1))[0]
+		maxKey = max(self._dict.iteritems(), key=operator.itemgetter(1))[0]
+		maxValue = self.get(maxKey)
+		return (maxKey, maxValue)
+		
 		
 	def getMin(self):
-		return min(self._dict.iteritems(), key=lambda x: x[1])
+		minKey = min(self._dict.iteritems(), key=lambda x: x[1])
+		return minKey
 		
 	def remove(self, id):
-		del self._dict[id]	
+		del self._dict[id]
+		
+	def clear(self):
+		self._dict.clear()
+		
+	def foreach(self, function, option):
+		for k, v in sorted(self._dict.items()):
+			function(k, v)
+			
+	def traversal(self, k, v):
+		print k, v
+		
+	def isEmpty(self):
+		return not self._dict
