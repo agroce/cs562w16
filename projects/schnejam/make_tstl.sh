049c8f3d23dcafbf802bd4c76b425e76af62f1e4
Jim Schneidereit <jimschneidereit@gmail.com>
2016-03-14 22:02:39 -0700

    adding some things

--- projects/schnejam/make_tstl.sh
+++ projects/schnejam/make_tstl.sh
@@ -2,4 +2,4 @@
 
 rm -rf sut.py
 tstl pyopencl.tstl -n
-python2.7 ~/tstl/generators/randomtester.py --maxtests=50 --nocover --depth=25
+python2.7 ~/tstl/generators/randomtester.py --maxtests=500 --nocover --depth=50
